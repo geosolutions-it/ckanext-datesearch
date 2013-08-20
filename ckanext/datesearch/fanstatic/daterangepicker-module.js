3b982640a7c48bb895746a6dd41b89a3bf5c702c
Sean Hammond <seanhammond@lavabit.com>
2013-08-20 15:13:14 +0200

    Improve the daterange search UX

--- ckanext/datesearch/fanstatic/daterangepicker-module.js
+++ ckanext/datesearch/fanstatic/daterangepicker-module.js
@@ -1,17 +1,42 @@
 this.ckan.module('daterangepicker-module', function($, _) {
     return {
         initialize: function() {
+
+            // Add hidden <input> tags #ext_startdate and #ext_enddate,
+            // if they don't already exist.
+            var form = $("#dataset-search");
+            if ($("#ext_startdate").length === 0) {
+                $('<input type="hidden" id="ext_startdate" name="ext_startdate" />').appendTo(form);
+            }
+            if ($("#ext_enddate").length === 0) {
+                $('<input type="hidden" id="ext_enddate" name="ext_enddate" />').appendTo(form);
+            }
+
+            // Add a date-range picker widget to the <input> with id #daterange
             $('input[id="daterange"]').daterangepicker({
                 showDropdowns: true,
                 timePicker: true
             },
             function(start, end) {
-                var q = 'metadata_modified:[';
-                q = q + start.format('YYYY-MM-DDTHH:mm:ss');
-                q = q + 'Z TO ';
-                q = q + end.format('YYYY-MM-DDTHH:mm:ss');
-                q = q + 'Z]';
-                $('input[class="search"]')[0].value = q;
+
+                // Bootstrap-daterangepicker calls this function after the user
+                // picks a start and end date.
+
+                // Format the start and end dates into strings in a date format
+                // that Solr understands.
+                start = start.format('YYYY-MM-DDTHH:mm:ss') + 'Z';
+                end = end.format('YYYY-MM-DDTHH:mm:ss') + 'Z';
+
+                // Set the value of the hidden <input id="ext_startdate"> to
+                // the chosen start date.
+                $('#ext_startdate').val(start);
+
+                // Set the value of the hidden <input id="ext_enddate"> to
+                // the chosen end date.
+                $('#ext_enddate').val(end);
+
+                // Submit the <form id="dataset-search">.
+                $("#dataset-search").submit();
             });
         }
     }
--- ckanext/datesearch/plugin.py
+++ ckanext/datesearch/plugin.py
@@ -4,7 +4,27 @@ import ckan.plugins.toolkit as toolkit
 
 class DateSearchPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_resource('fanstatic', 'ckanext-datesearch')
+
+    def before_search(self, search_params):
+        extras = search_params.get('extras')
+        if not extras:
+            # There are no extras in the search params, so do nothing.
+            return search_params
+        start_date = extras.get('ext_startdate')
+        end_date = extras.get('ext_enddate')
+        if not start_date or not end_date:
+            # The user didn't select a start and end date, so do nothing.
+            return search_params
+
+        # Add a date-range query with the selected start and end dates into the
+        # Solr facet queries.
+        fq = search_params['fq']
+        fq = '{fq} +metadata_modified:[{start_date} TO {end_date}]'.format(
+            fq=fq, start_date=start_date, end_date=end_date)
+        search_params['fq'] = fq
+        return search_params
