4f44dadd7863e24a0b5a4138be3d594598038078
Sean Hammond <seanhammond@lavabit.com>
2013-08-20 16:11:32 +0200

    Tweak date-range picker widget options

--- ckanext/datesearch/fanstatic/daterangepicker-module.js
+++ ckanext/datesearch/fanstatic/daterangepicker-module.js
@@ -14,6 +14,16 @@ this.ckan.module('daterangepicker-module', function($, _) {
 
             // Add a date-range picker widget to the <input> with id #daterange
             $('input[id="daterange"]').daterangepicker({
+                ranges: {
+                   'Today': [moment(), moment()],
+                   'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                   'Last 7 Days': [moment().subtract('days', 6), moment()],
+                   'Last 30 Days': [moment().subtract('days', 29), moment()],
+                   'This Month': [moment().startOf('month'), moment().endOf('month')],
+                   'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+                },
+                startDate: moment().subtract('days', 29),
+                endDate: moment(),
                 showDropdowns: true,
                 timePicker: true
             },
